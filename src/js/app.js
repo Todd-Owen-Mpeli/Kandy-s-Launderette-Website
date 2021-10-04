5b99370ba71f89d24627ef74724de29ca181f226
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-10-04 18:30:07 +0100

    Updated

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+/*===== NOTIFICATION DISPLAY =====*/
+function inStoreOptionDisplay() {
+	const showDisplay = document.getElementById("inStoregOption");
+	const displayStyles = document.getElementById("info");
+	const plusBtn = document.getElementById("inStoregOptionBtn");
+
+	// Changing Div styles
+	if (showDisplay.style.display === "none") {
+		showDisplay.style.display = "block";
+		displayStyles.style.color = "#fff";
+		displayStyles.style.minHeight = "5.5rem";
+		plusBtn.style.backgroundColor = "#ff0055";
+
+		// More Featured Projects Button
+		plusBtn.innerHTML = `<i class="bi bi-dash-lg"></i>`;
+	} else {
+		showDisplay.style.display = "none";
+		displayStyles.style.color = "#111";
+		plusBtn.style.backgroundColor = "#9d73ff";
+		// More Featured Projects Button
+		plusBtn.innerHTML = `<i class="bi bi-plus-lg"></i>`;
+	}
+}
+
+/*==================== QUESTIONNAIRE INFO ====================*/
+// Submit Button Listener
+inStoregOptionBtn.addEventListener("click", () => {
+	inStoreOptionDisplay();
+});
