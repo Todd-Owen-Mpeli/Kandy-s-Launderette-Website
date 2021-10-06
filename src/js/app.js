1038d25e488afe3fead5c9104e674f34d05be056
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-10-06 19:01:29 +0100

    Updated

--- src/js/app.js
+++ src/js/app.js
@@ -22,16 +22,61 @@ function inStoreOptionDisplay() {
 	}
 }
 
-/*==================== QUESTIONNAIRE INFO ====================*/
+/*==================== BUTTON LISTENER ====================*/
 // Submit Button Listener
 inStoregOptionBtn.addEventListener("click", () => {
 	inStoreOptionDisplay();
 });
 
 /*===== WEBSITE SCROLL REVEAL =====*/
-window.addEventListener("scroll", reveal);
 
-// Scroll Reveal Elements
-function reveal() {
-	const reveals = document.querySelectorAll(".reveal");
+/*==================== CONTACT US ====================*/
+
+//
+function validate() {
+	var name = document.getElementById("name").value;
+	var phone = document.getElementById("phone").value;
+	var email = document.getElementById("email").value;
+	var subject = document.getElementById("subject").value;
+	var message = document.getElementById("message").value;
+	var error_message = document.getElementById("error_message");
+
+	console.info(name);
+
+	// error_message.style.padding = "10px";
+
+	// var text;
+
+	// if (name.length < 3) {
+	// 	text = "Please Enter valid Name";
+	// 	error_message.innerHTML = text;
+	// 	return false;
+	// }
+
+	// if (isNaN(phone) || phone.length != 10) {
+	// 	text = "Please enter valid Phone Number";
+	// 	error_message.innerHTML = text;
+	// 	return false;
+	// }
+
+	// if (email.indexOf("@") == -1 || email.length < 6) {
+	// 	text = "Please Enter valid Email";
+	// 	error_message.innerHTML = text;
+	// 	return false;
+	// }
+
+	// if (subject.length < 10) {
+	// 	text = "Please Re-enter a Subject";
+	// 	error_message.innerHTML = text;
+	// 	return false;
+	// }
+
+	// if (message.length <= 140) {
+	// 	text = "Please Enter More Than 140 Characters";
+	// 	error_message.innerHTML = text;
+	// 	return false;
+	// }
+
+	// alert("Form Submitted Successfully!");
+	// return true;
 }
